73693b68c25019f290c9dd76f9ba49bf3584bb7c
Joshua Boyd <jaboydfamily@gmail.com>
2021-11-08 11:53:02 -0500

    added plane for ground fixed issue with renderer not getting correct window width

--- Basic-Scene/basicScene.js
+++ Basic-Scene/basicScene.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 	function loadScene() {
 		var scene = new THREE.Scene();
-		var camera = new THREE.PerspectiveCamera(45, window.innerWIdth/ window.innerHeight, 0.1, 1000);
-		var renderer = new THREE.WEBGLRenderer();
+		var camera = new THREE.PerspectiveCamera(45, window.innerWidth/ window.innerHeight, 0.1, 1000);
+		var renderer = new THREE.WebGLRenderer();
 		renderer.setClearColor(new THREE.Color(0x000000));
 		renderer.setSize(window.innerWidth, window.innerHeight);
+
+		var axes = new THREE.AxesHelper(20);
+		scene.add(axes);
+
+		// create a plane for the ground 
+		var planeGeo = new THREE.PlaneGeometry(50, 50);
+		var planeMat = new THREE.MeshBasicMaterial({
+			color: 0x56b00
+		});
+
+		var plane = new THREE.Mesh(planeGeo, planeMat);
+		plane.rotation.x = -0.5 * Math.PI;
+		plane.position.set(7, 0, -7);
+		scene.add(plane);
 
 		// position and point the camera to the center of the scene
 		camera.position.set(-30, 40, 30);
@@ -15,4 +29,6 @@ document.addEventListener('DOMContentLoaded', function(event) {
 		// render the scene
 		renderer.render(scene, camera);
 	}
+
+	loadScene();
 });
